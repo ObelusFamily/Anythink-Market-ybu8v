86cc21718cf011d38eadaf05bf4836a5f73a7213
Vincent <vince2848@gmail.com>
2022-08-04 07:35:20 -0600

    Filtering working with equal

--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -104,6 +104,7 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
         self,
         *,
         tag: Optional[str] = None,
+        title: Optional[str] = None,
         seller: Optional[str] = None,
         favorited: Optional[str] = None,
         limit: int = 20,
@@ -155,6 +156,17 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
                     )
                 ),
             )
+            # fmt: on
+            
+            
+        if title:
+            query_params.append(title)
+            query_params_count += 1
+
+            # fmt: off
+            query = query.select(query.star).where(
+                        items.title == Parameter(query_params_count),
+                    )
             # fmt: on
 
         if seller:
